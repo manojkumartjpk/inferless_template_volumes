e8356a45c54f5420505a099d29120000109e891e
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2025-08-06 16:06:43 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+import json
+import os
+import numpy as np
+import torch
+from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
+from datetime import datetime
+
+
+class InferlessPythonModel:
+
+    def initialize(self):
+        print("started load model", flush=True)
+        # Get custom cache path from environment
+        folder_path = os.getenv("NFS_PATH")
+        
+        # Set environment variable for Hugging Face cache
+        os.environ["TRANSFORMERS_CACHE"] = folder_path
+
+        # (Optional) set Torch hub cache too, if needed
+        os.environ["TORCH_HOME"] = folder_path
+
+        # Load the model using the cache path
+        self.generator = pipeline("text-generation", model="EleutherAI/gpt-neo-125M", device=0)
+
+
+    def infer(self, inputs):
+        print("started inference", flush=True)
+        prompt = inputs["prompt"]
+        pipeline_output = self.generator(prompt, do_sample=True, min_length=20)
+        generated_txt = pipeline_output[0]["generated_text"]
+        return {"generated_text": generated_txt }
+
+    def finalize(self, args):
+        self.generator = None
